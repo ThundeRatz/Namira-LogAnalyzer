ee0383f14b31d692f915a3e5c2ac8ec9742522be
mariarezende07 <maloasan@gmail.com>
2021-10-07 20:30:07 -0300

    ✨ Add prints for debug

--- loganalyzer/Testcase.py
+++ loganalyzer/Testcase.py
@@ -14,6 +14,7 @@ data = []
 results = []
 
 for i in li:
+    print("starting:",i)
     parser = Parser(i)
     game = Game(parser)
     analyzer = Analyzer(game)
@@ -25,9 +26,11 @@ for i in li:
         aux = [analyzer.agent_left_states[i]] + analyzer.agent_right_states[i] + [analyzer.ball_positions[i]]
         data.append([item for sublist in aux for item in sublist])
 
+    print("finishing:",i)
+
 data = np.transpose(data)
 
 np.savetxt("data.txt", data, fmt='%.4f')
 np.savetxt("results.txt", [results], fmt='%d')
 
-#L.x,L.y,L.stm,R.1.x,R.1.y,R.1.stm,R.2.x,R.2.y,R.2.stm,R.3.x,R.3.y,R.3.stm,R.4.x,R.4.y,R.4.stm,Ball.x,Ball.y,Ball.Vx,Ball.Vy
+#L.x,L.y,L.stm,R.1.x,R.1.y,R.1.stm,R.2.x,R.2.y,R.2.stm,R.3.x,R.3.y,R.3.stm,R.4.x,R.4.y,R.4.stm,Ball.x,Ball.y,Pass Angle
