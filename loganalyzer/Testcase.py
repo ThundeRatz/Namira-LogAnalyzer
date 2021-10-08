aa1d62f81c57f33afc12dbf349480346422c983e
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2021-10-07 21:10:25 -0300

    🎨 Format Testcase.py

--- loganalyzer/Testcase.py
+++ loganalyzer/Testcase.py
@@ -6,16 +6,17 @@ from Analyzer import *
 import numpy as np
 import os
 
-l = os.listdir(os.getcwd())
-li = [x.split('.')[0] for x in l if x.endswith(".rcg")]
+files = os.listdir(os.getcwd())
+logs = [x.split('.')[0] for file in files if file.endswith(".rcg")]
+logs = list(set(logs))
 
-li = list(set(li))
 data = []
 results = []
 
-for file in li:
-    print("starting:", file)
-    parser = Parser(file)
+for log in logs:
+    print("Starting:", log)
+
+    parser = Parser(log)
     game = Game(parser)
     analyzer = Analyzer(game)
     analyzer.analyze()
@@ -29,11 +30,11 @@ for file in li:
         except:
             continue
 
-    print("finishing:", file)
+    print("Finishing:", log)
 
 data = np.transpose(data)
 
 np.savetxt("data.txt", data, fmt='%.4f')
 np.savetxt("results.txt", [results], fmt='%d')
 
-#L.x,L.y,L.stm,R.1.x,R.1.y,R.1.stm,R.2.x,R.2.y,R.2.stm,R.3.x,R.3.y,R.3.stm,R.4.x,R.4.y,R.4.stm,Ball.x,Ball.y,Pass Angle
+#L.x,L.y,L.stm,R.1.x,R.1.y,R.1.stm,R.2.x,R.2.y,R.2.stm,R.3.x,R.3.y,R.3.stm,R.4.x,R.4.y,R.4.stm,Ball.x,Ball.y,Pass.Angle
