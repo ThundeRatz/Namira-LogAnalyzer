f3eab86e8ab9270ecd73380bbacc067b95d57042
RicardoHonda <ricardothonda@usp.br>
2023-03-11 17:59:52 -0300

    🚸 Improve .json output format

--- loganalyzer/utils.py
+++ loganalyzer/utils.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def write_json(save_path, dictionary):
     with open(save_path, 'w') as outfile:
-        json.dump(dictionary, outfile)
+        json.dump(dictionary, outfile, indent=4)
 
 def write_csv(save_path, list_of_lines, header=None):
     dataframe = pd.DataFrame(list_of_lines)
