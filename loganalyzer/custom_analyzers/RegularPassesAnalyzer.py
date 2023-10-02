0010686f8b2719b3e5b2f250811b41bf9c9d81e7
dasmieszneznaczki <erickdiogo2004@gmail.com>
2023-10-02 14:50:25 -0300

    ✨ Find missed passes and print accuracy

--- loganalyzer/custom_analyzers/RegularPassesAnalyzer.py
+++ loganalyzer/custom_analyzers/RegularPassesAnalyzer.py
@@ -6,6 +6,9 @@ class RegularPassesAnalyzer:
         self.pass_last_kicker = -1
         self.pass_last_kick_cycle = -1
         self.kick_states = []
+        self.missed_pass = False
+        self.missed_pass_count = 0
+        self.pass_count = 0
 
     @staticmethod
     def csv_headers():
@@ -63,13 +66,22 @@ class RegularPassesAnalyzer:
                     self.pass_status = 1
                     self.pass_last_kicker = self.game.get_last_kickers(cycle)[0]
                     self.pass_last_kick_cycle = cycle
-
+                    self.missed_pass = True
+                    self.missed_pass_count += 1
                 # Cycle is not exact. Most of the times it is the receiving cycle 
-                if (len(self.kick_states) == 0): # prevent off-by-one error
+                if len(self.kick_states) == 0: # prevent off-by-one error
                     self.kick_states.append([cycle, self.pass_last_kicker.number, 0])
-                elif (self.pass_last_kicker.number != self.kick_states[len(self.kick_states) - 1][1] and self.pass_last_kicker.team.name == self.game.left_team.name):
-                    self.kick_states[len(self.kick_states) - 1][2] = self.pass_last_kicker.number
+                    self.pass_count += 1
+                # Actual pass
+                elif self.pass_last_kicker.number != self.kick_states[len(self.kick_states) - 1][1] and self.pass_last_kicker.team.name == self.game.left_team.name:
+                    if not self.missed_pass:
+                        self.kick_states[len(self.kick_states) - 1][2] = self.pass_last_kicker.number
                     self.kick_states.append([cycle, self.pass_last_kicker.number, 0])
+                    self.missed_pass = False
+                    self.pass_count += 1
+                elif self.missed_pass: # Ball lost to enemy
+                    self.kick_states[len(self.kick_states) - 1][2] = -1
+
 
     def draw_heatmap(self):
         raise NotImplementedError(
@@ -80,3 +92,8 @@ class RegularPassesAnalyzer:
 
         for cycle in range(1, self.play_on_cycles[-1] + 1):
             self.check_pass(cycle)
+        
+        print(f"Total passes: {self.missed_pass_count + self.pass_count}")
+        print(f"Missed passes: {self.missed_pass_count}")
+        print(f"Actual passes: {self.pass_count}")
+        print(f"Total pass accuracy: {((100*self.pass_count)/(self.missed_pass_count + self.pass_count)):.2f}%")
